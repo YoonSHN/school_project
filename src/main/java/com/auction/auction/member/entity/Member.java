4ad1036cdd82b4244a39982aaf783f1522b1bc91
seunghyun <tkdenddl182@gmail.com>
2024-07-31 00:07:19 +0900

    member, product entity set

--- src/main/java/com/auction/auction/member/entity/Member.java
+++ src/main/java/com/auction/auction/member/entity/Member.java
@@ -1,8 +1,27 @@
 package com.auction.auction.member.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+
+
 
 }
--- src/main/java/com/auction/auction/product/entity/Product.java
+++ src/main/java/com/auction/auction/product/entity/Product.java
@@ -1,8 +1,27 @@
 package com.auction.auction.product.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private int currentprice; //현재 경마가
+    private int bid_increment; //입찰단가
+    private boolean ongoing; //경매중인지?
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
 
 }
