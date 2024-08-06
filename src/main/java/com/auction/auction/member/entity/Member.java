c8cb467874b5e8341b24ff9cd4ca3db652427740
seunghyun <tkdenddl182@gmail.com>
2024-08-06 20:08:33 +0900

    add base Entity

--- src/main/java/com/auction/auction/member/entity/Member.java
+++ src/main/java/com/auction/auction/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.auction.auction.member.entity;
 
+import com.auction.auction.market.base.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,18 +15,9 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class Member {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Member extends BaseEntity {
     private String username;
     private String password;
     private String email;
-
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
-
-
 
 }
--- src/main/java/com/auction/auction/product/entity/Product.java
+++ src/main/java/com/auction/auction/product/entity/Product.java
@@ -1,5 +1,6 @@
 package com.auction.auction.product.entity;
 
+import com.auction.auction.market.base.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,25 +15,12 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Product extends BaseEntity {
     private String name;
 
     private int price; //현재 경마가
     private int bid_increment; //입찰단가
     private boolean ongoing = false; //경매중인지?
-    @CreatedDate
-    private LocalDateTime createDate; //경매시작일
-    @LastModifiedDate
-    private LocalDateTime endDate;  //경매종료일
 
-    public void startAuction() {
-        this.ongoing = true;
-    }
 
-    public void endAuction() {
-        this.ongoing = false;
-    }
 }
