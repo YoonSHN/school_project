da3dac4943901ef08a8e11970944fb8d37e32007
seunghyun <tkdenddl182@gmail.com>
2024-08-29 18:19:49 +0900

    modify, delete,create authority

--- src/main/java/com/auction/auction/inquiry/controller/InquiryController.java
+++ src/main/java/com/auction/auction/inquiry/controller/InquiryController.java
@@ -9,9 +9,12 @@ import com.auction.auction.product.entity.Product;
 import com.auction.auction.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.Optional;
@@ -31,22 +34,30 @@ public class InquiryController {
         model.addAttribute("product", product);
         return "/inquiry/create";
     }
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
-    public String modify(@PathVariable("id")Long id, Model model){
+    public String modify(@PathVariable("id")Long id, Model model, Principal principal){
         Inquiry inquiry = inquiryService.getInquiry(id);
+
+        if(!inquiry.getMember().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
         model.addAttribute("inquiry",inquiry);
         return "inquiry/modify";
     }
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id")Long id){
+    public String delete(@PathVariable("id")Long id, Principal principal){
         Inquiry inquiry = inquiryService.getInquiry(id);
         inquiryService.delete(inquiry);
 
         Long productId = inquiry.getProduct().getId();
-
+        if(!inquiry.getMember().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+        }
         return String.format("redirect:/product/detail/%s", productId);
     }
-
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
     public String submit(@RequestParam("productId") Long productId,
                          Principal principal,
@@ -59,6 +70,7 @@ public class InquiryController {
         inquiryService.create(product, member,title, body);
         return "redirect:/product/detail/" +productId;
     }
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
     public String modify(@PathVariable("id") Long id,
                          Principal principal,
@@ -66,6 +78,9 @@ public class InquiryController {
         Inquiry inquiry = inquiryService.getInquiry(id);
         inquiryService.modify(inquiry, content);
         Long productId = inquiry.getProduct().getId();
+        if(!inquiry.getMember().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
 
         return String.format("redirect:/product/detail/%s", productId);
 
