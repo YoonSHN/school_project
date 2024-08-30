2145cfe7a39490f8d6f6f5a7de996e49a70a1f7f
seunghyun <tkdenddl182@gmail.com>
2024-08-30 22:28:58 +0900

    edit page and list search

--- src/main/java/com/auction/auction/product/repository/ProductRepository.java
+++ src/main/java/com/auction/auction/product/repository/ProductRepository.java
@@ -5,9 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAll(Pageable pageable);
+
+    @Query("""
+            select distinct p
+            from Product p
+            where p.name LIKE %:kw%
+            or p.script LIKE %:kw%
+            """)
+    Page<Product> findAllByKeyWord(@Param("kw")String kw, Pageable pageable);
 }
