dd946a8dd89008cb4737c3132cb207867d4f2e00
seunghyun <tkdenddl182@gmail.com>
2024-09-05 01:43:51 +0900

    cart list

--- src/main/java/com/auction/auction/cart/controller/CartController.java
+++ src/main/java/com/auction/auction/cart/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.auction.auction.cart.controller;
 
+import com.auction.auction.cart.entity.Cart;
 import com.auction.auction.cart.service.CartService;
 import com.auction.auction.member.entity.Member;
 import com.auction.auction.member.service.MemberService;
@@ -8,12 +9,14 @@ import com.auction.auction.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/cart")
@@ -25,7 +28,12 @@ public class CartController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/list")
-    public String list(){
+    public String list(Principal principal, Model model)
+    {
+        Member member = memberService.findByUserName(principal.getName());
+        List<Cart> cartList = cartService.getList(member);
+
+        model.addAttribute("cartList", cartList);
         return "cart/list";
     }
 
--- src/main/java/com/auction/auction/cart/repository/CartRepository.java
+++ src/main/java/com/auction/auction/cart/repository/CartRepository.java
@@ -1,9 +1,13 @@
 package com.auction.auction.cart.repository;
 
 import com.auction.auction.cart.entity.Cart;
+import com.auction.auction.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByMember(Member member);
 }
