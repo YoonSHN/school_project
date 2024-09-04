a447e807f96c05ceedc9ea9320aec14fb98a7f20
seunghyun <tkdenddl182@gmail.com>
2024-09-05 00:37:50 +0900

    add to cart feature

--- src/main/java/com/auction/auction/cart/controller/CartController.java
+++ src/main/java/com/auction/auction/cart/controller/CartController.java
@@ -1,19 +1,44 @@
 package com.auction.auction.cart.controller;
 
+import com.auction.auction.cart.service.CartService;
+import com.auction.auction.member.entity.Member;
+import com.auction.auction.member.service.MemberService;
+import com.auction.auction.product.entity.Product;
+import com.auction.auction.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/cart")
 @RequiredArgsConstructor
 public class CartController {
+    private final CartService cartService;
+    private final MemberService memberService;
+    private final ProductService productService;
+
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/list")
     public String list(){
         return "cart/list";
     }
+
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/add/{id}")
+    public String add(@PathVariable("id") Long id, Principal principal){
+        Product product = productService.getProduct(id);
+        Member member = memberService.findByUserName(principal.getName());
+
+        cartService.add(product, member);
+        return "redirect:/cart/list";
+    }
+
 
 }
--- src/main/java/com/auction/auction/cart/service/CartService.java
+++ src/main/java/com/auction/auction/cart/service/CartService.java
@@ -1,10 +1,27 @@
 package com.auction.auction.cart.service;
 
+import com.auction.auction.cart.entity.Cart;
+import com.auction.auction.cart.repository.CartRepository;
+import com.auction.auction.member.entity.Member;
+import com.auction.auction.product.entity.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
+    private final CartRepository cartRepository;
 
+
+    public void add(Product product, Member member) {
+        Cart cart = new Cart();
+        cart.setProduct(product);
+        cart.setMember(member);
+
+        cart.setCreateDate(LocalDateTime.now());
+
+        cartRepository.save(cart);
+    }
 }
